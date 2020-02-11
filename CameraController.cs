730720045f83875406e1e5d59b74a5f3e38f6204
Javier <takatsho@gmail.com>
2020-02-11 18:59:09 +0100

    Axis debug script. New mecanim implementation.

--- CameraController.cs
+++ CameraController.cs
@@ -5,6 +5,7 @@ public class CameraController : MonoBehaviour
     public Transform cameraOrbit;
     public Transform target;
     public float smoothTime = 1f;
+    public float rotationSpeed = 1f;
     private Vector3 velocity = Vector3.zero;
 
     void Start()
@@ -14,9 +15,14 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        cameraOrbit.position = Vector3.SmoothDamp(target.position, cameraOrbit.position, ref velocity, smoothTime);
+        cameraOrbit.position = Vector3.SmoothDamp(cameraOrbit.position, target.position, ref velocity, smoothTime);
         transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
 
-        transform.LookAt(target.position);
+        // transform.LookAt(target.position);
+
+        Vector3 direction = target.position - transform.position;
+        // Quaternion toRotation = Quaternion.FromToRotation(transform.forward, direction);
+        Quaternion toRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.time);
     }
 }
--- InputControl.cs
+++ InputControl.cs
@@ -1,45 +1,51 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class InputControl : MonoBehaviour {
-   public GameObject cameraOrbit;
+public class InputControl : MonoBehaviour
+{
+    public GameObject cameraOrbit;
 
-   public float rotateSpeed = 8f;
-   public float restViewAngle = 20f;
+    public float rotateSpeed = 8f;
+    public float restViewAngle = 20f;
 
-   void Update()
-   {
-       if (Input.GetMouseButton(0))
-       {
-            float h = rotateSpeed * Input.GetAxis("Mouse X");
-            float v = rotateSpeed * Input.GetAxis("Mouse Y");
+    void Update()
+    {
+        float inputX = Input.GetAxis("Axis 3");
+        float inputY = Input.GetAxis("Axis 4");
+        if (inputX != 0 || inputY != 0)
+        {
+            float h = rotateSpeed * inputX;
+            float v = rotateSpeed * inputY;
 
             float polarAngle = Vector3.Angle(Vector3.up, cameraOrbit.transform.forward);
             // Debug.Log("Angle: " + polarAngle);
 
-            if (polarAngle < 10f){
+            if (polarAngle < 10f)
+            {
                 v = 0;
             }
-            if (polarAngle > 170f){
+            if (polarAngle > 170f)
+            {
                 v = 0;
             }
 
             cameraOrbit.transform.eulerAngles = new Vector3(cameraOrbit.transform.eulerAngles.x + v, cameraOrbit.transform.eulerAngles.y + h, cameraOrbit.transform.eulerAngles.z);
         }
-        else {
+        else
+        {
             // Rotate the cube by converting the angles into a quaternion.
             Quaternion target = Quaternion.Euler(restViewAngle, cameraOrbit.transform.eulerAngles.y, 0);
 
             // Dampen towards the target rotation
-            cameraOrbit.transform.rotation = Quaternion.Slerp(cameraOrbit.transform.rotation, target,  Time.deltaTime * 10f);
+            cameraOrbit.transform.rotation = Quaternion.Slerp(cameraOrbit.transform.rotation, target, Time.deltaTime * 10f);
         }
 
-       float scrollFactor = Input.GetAxis("Mouse ScrollWheel");
+        float scrollFactor = Input.GetAxis("Mouse ScrollWheel");
 
-       if (scrollFactor != 0)
-       {
-           cameraOrbit.transform.localScale = cameraOrbit.transform.localScale * (1f - scrollFactor);
-       }
+        if (scrollFactor != 0)
+        {
+            cameraOrbit.transform.localScale = cameraOrbit.transform.localScale * (1f - scrollFactor);
+        }
 
-   }
+    }
 }
